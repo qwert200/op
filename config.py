0b13f73de1314fac361f69f3b594ff1373643f11
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2023-09-29 08:13:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https:",
+    "https:"https://github.com/Dev-senzir/seyid_senzir.git,
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(

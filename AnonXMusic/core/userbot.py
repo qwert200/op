b7ea8b7c2c324a3b9b2e026acf7eb382714c215e
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2023-10-03 16:44:16 +0300

    Update userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.one.join_chat("def_Zoka")
+                await self.one.join_chat("FH_EF")
             except:
                 pass
             assistants.append(1)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.three.join_chat("def_Zoka")
+                await self.one.join_chat("FH_EF")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.four.join_chat("def_Zoka")
+                await self.one.join_chat("FH_EF")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.five.join_chat("def_Zoka")
+                await self.one.join_chat("FH_EF")
             except:
                 pass
             assistants.append(5)

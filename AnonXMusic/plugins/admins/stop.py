3e31297dfeda532cd9e571ded708ca09ed41bc33
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2023-10-05 19:18:38 +0300

    Update stop.py

--- AnonXMusic/plugins/admins/stop.py
+++ AnonXMusic/plugins/admins/stop.py
@@ -10,7 +10,7 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["end", "stop", "cend", "cstop"]) & filters.group & ~BANNED_USERS
+    filters.command(["end", "stop", "cend", "cstop","ايقاف","انهاء"], "") & filters.group & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
@@ -21,3 +21,15 @@ async def stop_music(cli, message: Message, _, chat_id):
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
+
+@app.on_message(
+    filters.command(["ايقاف","/end","انهاء"], "") 
+    & filters.channel
+)
+async def stop_musicch(client, message):
+    chat_id = message.chat.id
+    if not len(message.command) == 1:
+        return
+    await Anony.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    await message.reply_text("❤️‍🔥 وقفت الاغاني لي يا نكدي \n\n❤️‍🔥 قـناه السـورس @TY_X_X")

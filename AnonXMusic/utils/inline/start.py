29c7c99c6b524d2fe4934c9d31e1ca8c09c10c7f
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2023-10-24 19:02:04 +0300

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -1,22 +1,25 @@
-from typing import Union
+from pyrogram.types import InlineKeyboardButton
 
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from AnonXMusic import app
 import config
+from AnonXMusic import app
+from config import BOT_USERNAME, OWNER, SUPPORT_CHANNEL
 
 
-def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+def start_panel(_):
     buttons = [
-        [
-            InlineKeyboardButton(
-                text="𝐒𝐎𝐔𝐑𝐂𝐄 𝗥َِ𝗲َِ𝗙َِ𝘇",
-                url=f"https://t.me/def_Zoka",
-            ),
-        ],
         [
             InlineKeyboardButton(
                 text="اضف البوت الى مجموعتك ✅",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="الـاوامر",
+                callback_data="settings_back_helper",
+            ),
+            InlineKeyboardButton(
+                text="المساعدة", callback_data="settings_helper"
             ),
         ],
      ]
@@ -27,22 +30,28 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], callback_data="settings_back_helper"
-            ),
-            InlineKeyboardButton(
-                text=_["S_B_88"], callback_data="hawl"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="• مطور البوت •", user_id=OWNER
-            )
-        ],
-        [
-            InlineKeyboardButton(
                 text="اضف البوت الى مجموعتك ✅",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
+        [
+            InlineKeyboardButton(
+                text="الاوامر", callback_data="settings_back_helper"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="لتنصيب بوت", url=f"https://t.me/IC_X_K"
+            ),
+            InlineKeyboardButton(
+                text="👤 مطور البوت", user_id=OWNER
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="قناة المطور", url=f"https://t.me/{SUPPORT_CHANNEL}"
+            )
+        ],
+      
      ]
     return buttons

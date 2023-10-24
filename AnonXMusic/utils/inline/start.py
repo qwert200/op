fa50ef92c0958186f5928d986c2e7edf4694150f
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2023-10-24 17:20:00 +0300

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -1,37 +1,48 @@
-from pyrogram.types import InlineKeyboardButton
+from typing import Union
 
-import config
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from AnonXMusic import app
+import config
 
 
-def start_panel(_):
+def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text="𝐒𝐎𝐔𝐑𝐂𝐄 𝗥َِ𝗲َِ𝗙َِ𝘇",
+                url=f"https://t.me/def_Zoka",
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-    ]
+        [
+            InlineKeyboardButton(
+                text="اضف البوت الى مجموعتك ✅",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            ),
+        ],
+     ]
     return buttons
 
 
-def private_panel(_):
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                text=_["S_B_1"], callback_data="settings_back_helper"
+            ),
+            InlineKeyboardButton(
+                text=_["S_B_88"], callback_data="hawl"
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text="• مطور البوت •", user_id=OWNER
+            )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
+            InlineKeyboardButton(
+                text="اضف البوت الى مجموعتك ✅",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
         ],
-    ]
+     ]
     return buttons

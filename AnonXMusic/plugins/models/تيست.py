a64964407fe579fbeb0c48823fba3951c442eb42
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2023-10-30 16:30:49 +0300

    Update تيست.py

--- AnonXMusic/plugins/models/تيست.py
+++ AnonXMusic/plugins/models/تيست.py
@@ -1,27 +1,21 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, ChatMemberStatus
 from AnonXMusic import app
 
 
-my_user_id = 6301863282
 
-# استخدم هذا الزر لربط الأمر بأمر الرفع
-@Client.on_message(filters.command("رفع_مشرف"))
-def promote_admin(client, message):
-    # تحقق مما إذا كان المستخدم هو المشرف بالفعل أم لا
-    if not message.from_user or not message.from_user.is_admin():
-        message.reply_text("عذرًا، يجب أن تكون مشرفًا لتنفيذ هذا الأمر.")
-        return
-    
-    # رفع المستخدم للمشرف باستخدام معرّف المستخدم ومعرّف المحادثة
-    client.promote_chat_member(
-        chat_id=message.chat.id,
-        user_id=my_user_id,
-        can_change_info=True,
-        can_delete_messages=True,
-        can_invite_users=True,
-        can_restrict_members=True,
-        can_pin_messages=True
-    )
-    message.reply_text("تم رفع المستخدم كمشرف بنجاح.")
+@app.on_message(filters.command('كتم') & filters.group)
+async def mute_user(client, message):
+    member = await app.get_chat_member(message.chat.id, message.from_user.id)
+    if member.status in [ChatMemberStatus.ADMINISTRATOR , ChatMemberStatus.OWNER]:
+        if message.reply_to_message and message.reply_to_message.from_user:
+            user_id = message.reply_to_message.from_user.id
+            chat_id = message.chat.id
 
+            await client.restrict_chat_member(chat_id, user_id, can_send_messages=False)
+
+            await message.reply(f"تم كتم المستخدم {user_id} في المجموعة.√")
+        else:
+            await message.reply("يرجى الرد على رسالة المستخدم الذي ترغب في كتمه.√")
+    else:
+        await message.reply("عذرًا، هذا الامر يخص المشرفين.√")

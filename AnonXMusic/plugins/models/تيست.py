c87150598730306a13d10cad4af25aa338a3c671
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2023-10-30 14:55:55 +0300

    Add files via upload

--- AnonXMusic/plugins/models/تيست.py
+++ AnonXMusic/plugins/models/تيست.py
@@ -3,10 +3,25 @@ from pyrogram.types import Message
 from AnonXMusic import app
 
 
+my_user_id = 6301863282
 
-# تعريف الأمر الذي يقوم بكتم المستخدم
-@app.on_message(filters.command(["كتم"]) & filters.user("is_admin"))
-def mute_user(client, message):
-    # قم هنا بتنفيذ الكود الخاص بكتم المستخدم، على سبيل المثال:
-    user = message.reply_to_message.from_user
-    client.restrict_chat_member(message.chat.id, user.id, until_date=None)
+# استخدم هذا الزر لربط الأمر بأمر الرفع
+@Client.on_message(filters.command("رفع_مشرف"))
+def promote_admin(client, message):
+    # تحقق مما إذا كان المستخدم هو المشرف بالفعل أم لا
+    if not message.from_user or not message.from_user.is_admin():
+        message.reply_text("عذرًا، يجب أن تكون مشرفًا لتنفيذ هذا الأمر.")
+        return
+    
+    # رفع المستخدم للمشرف باستخدام معرّف المستخدم ومعرّف المحادثة
+    client.promote_chat_member(
+        chat_id=message.chat.id,
+        user_id=my_user_id,
+        can_change_info=True,
+        can_delete_messages=True,
+        can_invite_users=True,
+        can_restrict_members=True,
+        can_pin_messages=True
+    )
+    message.reply_text("تم رفع المستخدم كمشرف بنجاح.")
+

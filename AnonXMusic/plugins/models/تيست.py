cb20207177cb7efffc0e3ad186e939e2b9aa5054
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2023-10-30 13:00:53 +0300

    Update تيست.py

--- AnonXMusic/plugins/models/تيست.py
+++ AnonXMusic/plugins/models/تيست.py
@@ -3,23 +3,21 @@ from pyrogram.types import Message
 from AnonXMusic import app
 
 
-# تعريف الأمر الذي يتم استدعاؤه لإلغاء تقييد المستخدم
-@app.on_message(filters.command('الغاء التقييد') & filters.group)
-async def unrestrict_user(client, message):
-    # التحقق مما إذا كان المرسل هو مشرف المجموعة
-    if message.from_user.is_admin:
-        # التحقق مما إذا كانت الرسالة تحتوي على رد على مستخدم
-        if message.reply_to_message and message.reply_to_message.from_user:
-            user_name = message.reply_to_message.from_user.name
-            chat_id = message.chat.id
+# تعريف المشرف
+admin_id = 6301863282
 
-            # إلغاء تقييد المستخدم في المجموعة
-            await client.restrict_chat_member(chat_id, user_name, can_send_messages=True)
 
-            await message.reply(f"تم إلغاء تقييد المستخدم {user_name} في المجموعة.")
-        else:
-            await message.reply("يرجى الرد على رسالة المستخدم الذي ترغب في إلغاء تقييده.")
-    else:
-        await message.reply("عذرًا، هذا الأمر متاح فقط للمشرفين.")
+
+
+# دالة للتحقق من صلاحيات المشرف
+def is_admin(user_id):
+    return user_id == admin_id
+
+
+# الأمر الذي يلزم صلاحيات المشرف لتشغيله
+@app.on_message(filters.command("الغاء التقييد", prefixes=".") & filters.user(is_admin))
+def unban_user(client: Client, message: Message):
+    # يتم هنا كتابة الكود الخاص بإلغاء تقييد المستخدم
+    # يمكنك استخدام ما تراه مناسبًا حسب متطلباتك
 
 

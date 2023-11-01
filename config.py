63a3af49ff67f7bd43221aed4e4a9b03f1a18daa
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2023-11-01 15:57:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "def_Zoka")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/def_Zoka")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/rev_fxx")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval

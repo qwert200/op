2d03ebb3ae71407b5cf901385c33555c277c56c0
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2023-11-05 18:40:16 +0300

    Update يارب.py

--- AnonXMusic/plugins/models/يارب.py
+++ AnonXMusic/plugins/models/يارب.py
@@ -1,14 +1,14 @@
-from pyrogram import Cilent
-from pyrogram import filtres
+from pyrogram import Client 
+from pyrogram import filters
 from AnonXMusic import app
 
 #test
 @app.on_message(filtres.command('سيارات'))
 def command(app, message):
-	app.send_photo( message.chat.id, "https://imgur.com/gallery/ZNwYBBz")
-	app.send_photo( message.chat.id, "https://imgur.com/gallery/jydi4")
-	app.send_photo( message.chat.id, "https://imgur.com/gallery/12a9s")
-	app.send_photo( message.chat.id, "https://imgur.com/gallery/YDhvc")
-	app.send_photo( message.chat.id, "https://imgur.com/gallery/YeSt0qU")
-	app.send_photo( message.chat.id, "https://imgur.com/gallery/IyEp7mf")
-	app.send_photo( message.chat.id, "https://imgur.com/gallery/gzgaon6")
+ app.send_photo( message.chat.id, "https://imgur.com/gallery/ZNwYBBz")
+ app.send_photo( message.chat.id, "https://imgur.com/gallery/jydi4")
+ app.send_photo( message.chat.id, "https://imgur.com/gallery/12a9s")
+ app.send_photo( message.chat.id, "https://imgur.com/gallery/YDhvc")
+ app.send_photo( message.chat.id, "https://imgur.com/gallery/YeSt0qU")
+ app.send_photo( message.chat.id, "https://imgur.com/gallery/IyEp7mf")
+ app.send_photo( message.chat.id, "https://imgur.com/gallery/gzgaon6")

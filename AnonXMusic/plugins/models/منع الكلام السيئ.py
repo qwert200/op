b03173cf773a854405e7f378fb9dd7240c142a9e
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2023-11-06 19:02:01 +0300

    Add files via upload

---
+++ AnonXMusic/plugins/models/منع الكلام السيئ.py
@@ -0,0 +1,14 @@
+from pyrogram import Cilent
+from pyrogram import filters
+from AnonXMusic import app
+
+#منع الكلام السيئ
+@app.on_message(filters.text)
+def delete_text(app.message):
+	word_list - ["سكس", "منتاك"]
+	if message.text in word_list:
+	app.delete_messages(message.chat.id, message.message_id)
+	app.send_message(message.chat.id, "لاتسب يورع")
+	#𝙎 .࿆𝙉 .࿆𝙍 </>
+	#T.me/programer_senzir
+	#سينزر الهيبه يولد

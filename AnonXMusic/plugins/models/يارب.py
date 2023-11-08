c063c97c02e95da5c7a9a5771872dd62267f2a67
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2023-11-08 15:50:48 +0300

    Update يارب.py

--- AnonXMusic/plugins/models/يارب.py
+++ AnonXMusic/plugins/models/يارب.py
@@ -3,12 +3,46 @@ from pyrogram import filters
 from AnonXMusic import app
 
 #test
-@app.on_message(filters.command('سيارات'))
+@app.on_message(filters.command("سيارات",""))
+def command(app, message):
+  app.send_photo( message.chat.id, "https://imgur.com/gallery/gzgaon6")
+ 
+#test
+@app.on_message(filters.command("سيارات",""))
+def command(app, message):
+ app.send_photo( message.chat.id, "https://imgur.com/gallery/jydi4")
+
+#test
+@app.on_message(filters.command("سيارات",""))
+def command(app, message):
+ app.send_photo( message.chat.id, "https://imgur.com/gallery/12a9s")
+
+#test
+@app.on_message(filters.command("سيارات",""))
 def command(app, message):
  app.send_photo( message.chat.id, "https://imgur.com/gallery/ZNwYBBz")
- app.send_photo( message.chat.id, "https://imgur.com/gallery/jydi4")
- app.send_photo( message.chat.id, "https://imgur.com/gallery/12a9s")
+ 
+ #test
+@app.on_message(filters.command("سيارات",""))
+def command(app, message):
  app.send_photo( message.chat.id, "https://imgur.com/gallery/YDhvc")
+
+ #test
+@app.on_message(filters.command("سيارات",""))
+def command(app, message):
  app.send_photo( message.chat.id, "https://imgur.com/gallery/YeSt0qU")
- app.send_photo( message.chat.id, "https://imgur.com/gallery/IyEp7mf")
- app.send_photo( message.chat.id, "https://imgur.com/gallery/gzgaon6")
+
+ #test
+@app.on_message(filters.command("سيارات",""))
+def command(app, message):
+ app.send_photo( message.chat.id, "https://imgur.com/gallery/YeSt0qU")
+
+ #test
+@app.on_message(filters.command("سيارات",""))
+def command(app, message):
+  app.send_photo( message.chat.id, "https://imgur.com/gallery/IyEp7mf")
+
+ #test
+@app.on_message(filters.command("سيارات",""))
+def command(app, message):
+  app.send_photo( message.chat.id, "https://imgur.com/gallery/IyEp7mf")

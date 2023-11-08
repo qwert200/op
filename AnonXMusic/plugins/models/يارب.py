395e5a978aa71cd6c733af25755a1688a59d355d
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2023-11-08 15:57:06 +0300

    Update يارب.py

--- AnonXMusic/plugins/models/يارب.py
+++ AnonXMusic/plugins/models/يارب.py
@@ -2,47 +2,47 @@ from pyrogram import Client
 from pyrogram import filters
 from AnonXMusic import app
 
-#test
+#𝙎 .࿆𝙉 .࿆𝙍 </>
 @app.on_message(filters.command("سيارات",""))
 def command(app, message):
   app.send_photo( message.chat.id, "https://imgur.com/gallery/gzgaon6")
  
-#test
+#𝙎 .࿆𝙉 .࿆𝙍 </>
 @app.on_message(filters.command("سيارات",""))
 def command(app, message):
  app.send_photo( message.chat.id, "https://imgur.com/gallery/jydi4")
 
-#test
+#𝙎 .࿆𝙉 .࿆𝙍 </>
 @app.on_message(filters.command("سيارات",""))
 def command(app, message):
  app.send_photo( message.chat.id, "https://imgur.com/gallery/12a9s")
 
-#test
+#𝙎 .࿆𝙉 .࿆𝙍 </>
 @app.on_message(filters.command("سيارات",""))
 def command(app, message):
  app.send_photo( message.chat.id, "https://imgur.com/gallery/ZNwYBBz")
  
- #test
+ #𝙎 .࿆𝙉 .࿆𝙍 </>
 @app.on_message(filters.command("سيارات",""))
 def command(app, message):
  app.send_photo( message.chat.id, "https://imgur.com/gallery/YDhvc")
 
- #test
+ #𝙎 .࿆𝙉 .࿆𝙍 </>
+@app.on_message(filters.command("سيارات",""))
+def command(app, message):
+ app.send_photo( message.chat.id, "https://imgur.com/gallery/Z0veA")
+
+ #𝙎 .࿆𝙉 .࿆𝙍 </>
 @app.on_message(filters.command("سيارات",""))
 def command(app, message):
  app.send_photo( message.chat.id, "https://imgur.com/gallery/YeSt0qU")
 
- #test
-@app.on_message(filters.command("سيارات",""))
-def command(app, message):
- app.send_photo( message.chat.id, "https://imgur.com/gallery/YeSt0qU")
-
- #test
+ #𝙎 .࿆𝙉 .࿆𝙍 </>
 @app.on_message(filters.command("سيارات",""))
 def command(app, message):
   app.send_photo( message.chat.id, "https://imgur.com/gallery/IyEp7mf")
 
- #test
+ #𝙎 .࿆𝙉 .࿆𝙍 </>
 @app.on_message(filters.command("سيارات",""))
 def command(app, message):
-  app.send_photo( message.chat.id, "https://imgur.com/gallery/IyEp7mf")
+  app.send_photo( message.chat.id, "https://imgur.com/gallery/hOjnc")

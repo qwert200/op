188aa468964f8a1eec31dfc105b64ec29bbf3822
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2023-11-09 14:31:30 +0300

    Update رتبتي.py

--- AnonXMusic/plugins/models/رتبتي.py
+++ AnonXMusic/plugins/models/رتبتي.py
@@ -3,11 +3,11 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from AnonXMusic import app
 
-
+#S↝N↝R
 @app.on_message(filters.command("رتبتي", ""))
 async def rotba(_: Client, message: Message):
     user_id = message.from_user.id 
-    member = message.chat.get_chat_member(user_id)
+    member = await app.get_chat_member(message.chat.id ,user_id)
     if member.status == ChatMemberStatus.MEMBER: return await message.reply("- رتبتك هي العضو.", reply_to_message_id=message.id)
     elif member.status == ChatMemberStatus.ADMINISTRATOR: return await message.reply("- رتبتك هي الادمن.", reply_to_message_id=message.id)
     elif member.status == ChatMemberStatus.OWNER: return await message.reply("- رتبتك هي المالك.", reply_to_message_id=message.id)
